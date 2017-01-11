0ab4423330146f362733c148ec28bd59ba8cf7fa
Tim <timothyhsieh27@gmail.com>
2017-01-11 18:21:12 -0500

    Updated function to accept strings of numbers; program will not permit users to submit pure integers as inputs.

--- user_input_statistics.rb
+++ user_input_statistics.rb
@@ -1,7 +1,7 @@
 def check_if_number(input)
-  string_input = input.to_s
-  number_input = string_input.to_f
-  input == number_input
+  number_input = input.to_i
+  string_input = number_input.to_s
+  input == string_input
 end
 
 def store_number(input, inputs_array)
