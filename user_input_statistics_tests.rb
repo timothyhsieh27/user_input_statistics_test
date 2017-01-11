7120eba65d38a163fd860d8fd8c88375cca2537f
Tim <timothyhsieh27@gmail.com>
2017-01-11 18:22:45 -0500

    Uncommented remaining tests.

--- user_input_statistics_tests.rb
+++ user_input_statistics_tests.rb
@@ -8,24 +8,24 @@ class TestUserInputStatistics < Minitest::Test
     assert_equal(FALSE, check_if_number('A'))
   end
 
-  # def test_if_number_stored?
-  #   assert_equal([1], store_number(1, []))
-  #   assert_equal([1, 2], store_number(2, [1]))
-  #   assert_equal([2, 1], store_number(1, [2]))
-  # end
-  #
-  # def test_if_numbers_counted?
-  #   assert_equal(1, count_numbers([0]))
-  #   assert_equal(2, count_numbers([1, 2]))
-  # end
-  #
-  # def test_if_numbers_summed?
-  #   assert_equal(6, sum_numbers([1, 2, 3]))
-  #   assert_equal(1, sum_numbers([0, 1]))
-  # end
-  #
-  # def test_if_averaged?
-  #   assert_equal(1.0, average_numbers([1, 1, 1]))
-  #   assert_equal(2.0, average_numbers([1, 3]))
-  # end
+  def test_if_number_stored?
+    assert_equal([1], store_number(1, []))
+    assert_equal([1, 2], store_number(2, [1]))
+    assert_equal([2, 1], store_number(1, [2]))
+  end
+
+  def test_if_numbers_counted?
+    assert_equal(1, count_numbers([0]))
+    assert_equal(2, count_numbers([1, 2]))
+  end
+
+  def test_if_numbers_summed?
+    assert_equal(6, sum_numbers([1, 2, 3]))
+    assert_equal(1, sum_numbers([0, 1]))
+  end
+
+  def test_if_averaged?
+    assert_equal(1.0, average_numbers([1, 1, 1]))
+    assert_equal(2.0, average_numbers([1, 3]))
+  end
 end
