276069b124544d50b440a12c4aa727def3909ac3
Tim <timothyhsieh27@gmail.com>
2017-01-11 13:20:29 -0500

    Wrote tests to verify that only numbers would pass the check_if_number function.

--- user_input_statistics_tests.rb
+++ user_input_statistics_tests.rb
@@ -1,5 +1,14 @@
 require 'minitest/autorun'
-require relative 'user_input_statistics'
+require_relative 'user_input_statistics'
+#
+class TestUserInputStatistics < Minitest::Test
+  def test_if_number
+    assert_equal(TRUE, check_if_number(1))
+    assert_equal(FALSE, check_if_number('1'))
 
-class TestUserInputStatistics < Minitest::TestUserInputStatistics
+    assert_equal(TRUE, check_if_number(0))
+    assert_equal(FALSE, check_if_number('0'))
+
+    assert_equal(FALSE, check_if_number('A'))
+  end
 end
