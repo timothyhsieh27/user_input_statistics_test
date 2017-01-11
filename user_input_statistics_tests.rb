aa568bdfd6df81d557489742474fb5d7edae8777
Tim <timothyhsieh27@gmail.com>
2017-01-11 13:35:43 -0500

    Wrote test to verify that input was successfully added into inputs array.

--- user_input_statistics_tests.rb
+++ user_input_statistics_tests.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative 'user_input_statistics'
 #
 class TestUserInputStatistics < Minitest::Test
-  def test_if_number
+  def test_if_number?
     assert_equal(TRUE, check_if_number(1))
     assert_equal(FALSE, check_if_number('1'))
 
@@ -10,5 +10,11 @@ class TestUserInputStatistics < Minitest::Test
     assert_equal(FALSE, check_if_number('0'))
 
     assert_equal(FALSE, check_if_number('A'))
+  end
+
+  def test_if_number_stored?
+    assert_equal([1], store_number(1, []))
+    assert_equal([1, 2], store_number(2, [1]))
+    assert_equal([2, 1], store_number(1, [2]))
   end
 end
